06634504a5129e229ffd03978aefdb1688b06c4c
bavshehata <bavshehata@gmail.com>
2021-03-09 11:19:18 +0200

    Writing to a JSON file

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
-const fs = require("fs").promises;
+// // Reading from a JSON file
 
-const readMyFile = async (link) =>{
-    var data = await fs.readFile(link,'utf8');
-    data = JSON.parse(data);
-    data.forEach(db => {
-        console.log(`${db.name} : ${db.type}`);
-    });
-}
+// // Async reading
 
-readMyFile("test.json");
+const fs = require('fs').promises;
 
+// const readMyFile = async (file) => {
+//   try {
+//     var data = await fs.readFile(file, 'utf8');
+//     data = JSON.parse(data);
+//     data.forEach((db) => {
+//       console.log(`${db.name} : ${db.type}`);
+//     });
+//   } catch (err) {
+//     console.log("Couldn't read file: \n", err);
+//   }
+// };
 
-// OR (This is a sync method and should only be used with almost static databases)
+// readMyFile('test.json');
 
-const databases = require('./test.json');
+// // OR (This is a sync method and should only be used with almost static databases)
 
-// print all databases
-databases.forEach(db => {
-    console.log(`${db.name}: ${db.type}`);
-});
+// const databases = require('./test.json');
+
+// // print all databases
+// databases.forEach((db) => {
+//   console.log(`${db.name}: ${db.type}`);
+// });
+
+// // Writing to a JSON file
+
+// Async writing
+
+var database = {
+  name: 'mongo',
+  type: 'noSQL',
+};
+
+const writeToFile = async (file) => {
+  try {
+    database = JSON.stringify(database, null, 2);
+    await fs.writeFile(file, database, 'utf8');
+    console.log('Data written successfully');
+  } catch (e) {
+    console.log("couldn't write to file\n", e);
+  }
+};
+
+writeToFile('./test.json');
+
+// Same idea can be done with sync
